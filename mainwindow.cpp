1f869109108c7967140fb5d9f4b36a19aeb9bce3
kolesnykanton <kolesnyk.antony@gmail.com>
2017-02-26 01:40:15 +0200

    Project added

---
+++ mainwindow.cpp
@@ -0,0 +1,81 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include "qdialogbuttonbox.h"
+#include "QFlags"
+#include "QModelIndex"
+#include "qlistwidget.h"
+#include "string"
+#include "qabstractitemview.h"
+#include "QSpinBox"
+#include <iostream>
+#include <QTextStream>
+#include <stdio.h>
+#include <QProcess>
+
+using namespace std;
+
+
+string command;
+string hiber = " /nobreak & shutdown /h";
+string force = "/f";
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->buttonBox, SIGNAL(clicked (QAbstractButton*)), this, SLOT(buttonsClicked(QAbstractButton*)));
+    connect(ui->listWidget,SIGNAL(itemClicked(QListWidgetItem*)),this,SLOT(itemClicked(QListWidgetItem*)));
+    connect(ui->spinBox, SIGNAL(valueChanged(int)),this, SLOT(on_spinboxValueChanged(int)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+}
+
+
+void MainWindow::on_spinBox_valueChanged(int arg1)
+{
+    timeout = arg1;
+}
+
+void MainWindow::on_buttonBox_clicked(QAbstractButton *button)
+{
+    if(ui->buttonBox->standardButton(button) == QDialogButtonBox::Close) {
+        close();
+        }
+    else if(ui->buttonBox->standardButton(button) == QDialogButtonBox::Ok) {
+        //system(command.c_str());
+
+        }
+    else {
+        system("shutdown -a");
+        }
+}
+
+
+
+void MainWindow::on_listWidget_itemClicked(QListWidgetItem *item)
+{
+    if(item->text()=="Shutdown"){
+            command = "shutdown /s /t " + to_string(ui->spinBox->value());
+    }
+    else if(item->text()=="Hibernate"){
+        command = "timeout /t " + to_string(ui->spinBox->value()) + hiber;
+    }
+}
+
+
+
+
+
+void MainWindow::on_comboBox_currentIndexChanged(int index)
+{
+    if(index == 0){
+        hourtime = ui->spinBox->value() * 3600;
+    }
+}
